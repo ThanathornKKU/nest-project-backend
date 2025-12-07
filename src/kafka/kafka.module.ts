21c60cca15769302639ed3da9cf9b9ec0c621e17
ThanathornKKU <thanathorn.c@kkumail.com>
2025-12-07 13:42:48 +0700

    edit kafka module

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,15 +1,3 @@
-// // src/kafka/kafka.module.ts
-
-// import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
-// import { KafkaProducerService } from './kafka.producer.service';
-
-// @Module({
-//   providers: [KafkaProducerService],
-//   exports: [KafkaProducerService],
-// })
-// export class KafkaModule {}
-
 import { Module } from '@nestjs/common';
 import { KafkaService } from './kafka.producer.service';
 
